63b96b1b772fc1df033676f7bb200105fc0998f6
Chris Nguyen <cnguyen11@luc.com>
2022-09-28 23:42:17 -0500

    Clarify comments

--- encodeSequence.py
+++ encodeSequence.py
@@ -5,9 +5,9 @@ import numpy as np
 def one_hot (sequence):
     aa_alphabet = ['A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V']
     encoding = [[ 
-        1 if seq == aa else 0 # Note if seq is not in aa_alphabet returns 0 for all which is V
-            for aa in aa_alphabet[:-1] # avoid colinearity, trap
+        1 if seq == aa else 0 # if not in aa_alphabet, assumes to be V ... should more into
+            for aa in aa_alphabet[:-1] # avoid collinearity trap
                 ] for seq in sequence.upper() 
                 ] 
-    return [aa for postion in encoding for aa in postion] # flatten 
+    return [aa for postion in encoding for aa in postion] # flatten into a single list
 
