2cb80406d4d2b3704f3e0cda1a6fe68404a32970
Chris Nguyen <cnguyen11@luc.com>
2022-09-30 21:11:57 -0500

    fixed one hot encoding for aa not in list

--- encodeSequence.py
+++ encodeSequence.py
@@ -5,7 +5,7 @@ import numpy as np
 def one_hot (sequence):
     aa_alphabet = ['A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V']
     encoding = [[ 
-        1 if seq == aa else 0 # if not in aa_alphabet, assumes to be V ... should more into
+        1 if seq == aa else (1/20 if seq not in aa_alphabet else 0) # if not in aa_alphabet, impute as the mean 
             for aa in aa_alphabet[:-1] # avoid collinearity trap
                 ] for seq in sequence.upper() 
                 ] 
